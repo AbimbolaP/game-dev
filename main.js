604efd8dbc1cee4cdb76548c430501668c5c4384
AbimbolaP <abimbola.peace131@gmail.com>
2026-01-21 12:27:59 +0100

    folder structure

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function(){
+  const canvas = this.document.getElementById('canvas1');
+  canvas.width = 500;
+  canvas.height = 500;
+
+
+  class Game{
+    constructor(width, height){
+      this.width= width;
+      this.height =  height
+    }
+    update(){
+      
+    }
+    draw(){
+
+    }
+
+  }
+});
